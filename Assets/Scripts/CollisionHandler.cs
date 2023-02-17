de62d57fdbf230f66acfee604bc726366848cd88
Gatemaster64 <daniel.roestenburg01@outlook.com>
2023-02-17 17:12:28 +0100

    Added a note

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -25,7 +25,8 @@ public class CollisionHandler : MonoBehaviour
         }
     }
 
-
+    // Methods to reload the level & next level.
+   
      void ReloadLevel()
     {   //  Declaring a variable for the active scene, and loading the scene & Index.
         int currentSceneIndex = (SceneManager.GetActiveScene().buildIndex);
@@ -33,7 +34,8 @@ public class CollisionHandler : MonoBehaviour
     }
 
     void LoadNextLevel()
-    {
+    {    // if the level is the last in the index it will reload the first level in the index.
+        
         int currentSceneIndex = (SceneManager.GetActiveScene().buildIndex);
         int nextSceneIndex = currentSceneIndex + 1;
         if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
