f30f18c5f892cbe3de1de9648be9f53d2e22552a
Gatemaster64 <daniel.roestenburg01@outlook.com>
2023-02-23 18:28:02 +0100

    Updated Oscilator script to prevent NAN error

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -21,8 +21,9 @@ public class Oscillator : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()                              
     {
+        if (period <=  Mathf.Epsilon) { return; }     
         float cycles = Time.time / period; // continually growing over time
 
         
